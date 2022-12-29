321272fcca0dcd1e1fbc762d02526ab12ed77f8e
njuohlot <njuohlottimngum@gmail.com>
2022-12-29 07:17:05 +0100

    new changes

--- src/App.js
+++ src/App.js
@@ -4,34 +4,10 @@ import React  from "react";
 
 function App () {
 
-    const [count, setCount] = useState(3);
-    const [count1, setCount1] = useState(9);
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
-    const [texterae, setTextarea] = useState("");
-
-    const handle= (event)=>{
-        event.preventDefault();
-        setTextarea(event.target.value);
-
-    }
+  
     return(
         <div className="App">
-            <p>{count}</p>
-            <p>{count1}</p>
-            <button onClick={()=>{
-                setCount( c=> c + 1);
-                setCount1(a => a + 1)}
-                }>+</button>
-                <br/>
-                <input type='email' onChange={(e) =>setEmail(e.target.value)} placeholder='email' value={email}/>
-                <br/>
-                <input type='password' onChange={(e) =>setPassword(e.target.value)} placeholder='password' value={password}/>
-                 
-                 <textarea onChange={handle}></textarea>
-                 <p>{email}</p>
-                 <p>{password}</p>
-                 <p>{texterae}</p>
+            <p>My first deployed aws amplify application</p>
         </div>
     )
 }
